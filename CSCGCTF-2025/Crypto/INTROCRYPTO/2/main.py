67f038a4b638b79d8e2454476b33e8111c8d57ef
Minh Duc <kyshoza5555@gmail.com>
2025-07-29 12:41:03 +0700

    Delete CSCGCTF-2025 directory

--- CSCGCTF-2025/Crypto/INTROCRYPTO/2/main.py
+++
@@ -1,8 +0,0 @@
-def hex_to_ascii(hex_string):
-    bytes_object = bytes.fromhex(hex_string)
-    ascii_string = bytes_object.decode("ascii", errors="ignore")
-    return ascii_string
-
-# Example usage
-hex_string = "0B05041583000000030103013000480E01FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFBFFF9FFFFF3FFC71FFFFFFDFFFEFBFFDED0F32"
-print(hex_to_ascii(hex_string))
